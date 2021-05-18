17157cf7707238890050503eee3150241a8450ff
Ex-tela <81362694+Ex-tela@users.noreply.github.com>
2021-05-18 11:37:11 +0100

    Update chord_server.py

--- chord_server.py
+++ chord_server.py
@@ -11,11 +11,6 @@ def format_chordpro():
    chord = request.data.decode()
    return chord_formatter.format(chord)
 
-    
-# @app.route('/', methods=['GET'])
-# def root_home():
-    # return app.send_static_file('home_layout.html')
-
 
 @app.route('/')
 def root_home(name=None):
