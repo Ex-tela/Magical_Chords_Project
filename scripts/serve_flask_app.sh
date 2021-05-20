fa84fa6633e7e69566870963c06d1154de84eb38
Estela <edelvallemininel@gmail.com>
2021-05-20 22:58:17 +0100

    update

--- scripts/serve_flask_app.sh
+++ scripts/serve_flask_app.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+set -e # abort if any command fails
+
 export FLASK_APP=magical_chords
 export FLASK_ENV=development
 
+echo "Creating database"
+flask init-db
 echo "Executing flask service"
 flask run -h 0.0.0.0  
