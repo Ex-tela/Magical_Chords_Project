19fec66a66197549af3b2606c1e4e60fcce6793b
Estela <edelvallemininel@gmail.com>
2021-05-25 15:37:51 +0100

    database added

--- src/magical_chords/__init__.py
+++ src/magical_chords/__init__.py
@@ -3,18 +3,41 @@ from flask import render_template
 import flask
 from flask import request
 from magical_chords import chord_formatter
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = flask.Flask(__name__, static_url_path='/web/')
 app.config["DEBUG"] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/magical_chords'
+
+db = SQLAlchemy(app)
+
+class Chords(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(300), nullable=False)
+    song_content = db.Column(db.Text, nullable=False)
 
 
 # database creation
 @click.command('init-db')
 def init_db():
-    print('hello_world!')
+    db.create_all()
+
+    num_songs = Chords.query.count()
+    if num_songs == 0:
+        song1 = Chords(author='The Beatles', title="We Can Work It Out", song_content="We Can[D] Work It [G]Out")
+        song2 = Chords(author='Jefferson Airplane', title="Somebody to Love", song_content="Don't you[D] need [G]somebody to lo[C]ve?")
+    
+        db.session.add(song1)
+        db.session.add(song2)
+        db.session.commit()
+
 
 app.cli.add_command(init_db)
+# this regiters the function so shell can recognize it
 
 
 # chord formatter "import"
@@ -35,10 +58,9 @@ def root_about(name=None):
 
 @app.route('/library/', methods=['GET'])
 def root_library():
-    return render_template('library_layout.html')
-
+    songs = Chords.query.all()
+    return render_template('library_layout.html', songs=songs)
 
 
 if __name__ == "__main__":
     app.run()
-
